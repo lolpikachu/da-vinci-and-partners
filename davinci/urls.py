749a2210a66fdfb1fe991907aa24dfa84513388c
Lika <anzhelikadarijchuk@gmail.com>
2020-10-16 23:06:39 +0200

    design changes

--- davinci/urls.py
+++ davinci/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index
+from .views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('about/', about),
 ]
 
 if settings.DEBUG:
--- davinci/views.py
+++ davinci/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', {})
-    # return render(request, 'index_hr.html', {})
+    # return render(request, 'index.html', {})
+    return render(request, 'index_hr.html', {})
     # return render(request, 'index_violet_background.html', {})
     # return render(request, 'index_with_number_menu.html', {})
     # return render(request, 'index_spline_design.html', {})
 
-# def blog(request):
-#     return render(request, 'blog.html', {})
+def about(request):
+    return render(request, 'star.html', {})
 
 
