911f07fe9bfc1a1add4e6cd3837e71022e3af71a
Lika <anzhelikadarijchuk@gmail.com>
2020-11-05 03:04:31 +0100

    static collected

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -30,151 +30,198 @@ $(window).scroll(function() {
 }).scroll();
 // END background color chnage on scroll 
 
-// egg toggle 
-var select = function(s) {
-  return document.querySelector(s);
-},
-selectAll = function(s) {
-  return document.querySelectorAll(s);
-}, 
-animationWindow = select('#animationWindow'),    
-  animData = {
-  wrapper: animationWindow,
-  animType: 'svg',
-  loop: false,
-  prerender: false,
-  autoplay: false,
-  path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/egg_flip.json'
-}, anim;
 
-anim = bodymovin.loadAnimation(animData);
-anim.addEventListener('DOMLoaded', onDOMLoaded);
-anim.setSpeed(15);
-
-function onDOMLoaded(e){
-
-animationWindow.onclick = function(e){
-if(anim.currentFrame > 0){
- anim.playSegments([anim.currentFrame, 0], true);
- TweenMax.to('.eggGroup', 1, {
-  x:0,
-  ease:Elastic.easeOut.config(0.9,0.38)
- })   
-} else {
- TweenMax.to('.eggGroup', 1.4, {
-  x:73,
-  ease:Elastic.easeOut.config(0.9,0.38)
- })
- anim.playSegments([anim.currentFrame, 300], true)
-}
-}
-}
-
-// END egg toggle
-
-
-
-var w = window.innerWidth, h = window.innerHeight;
-window.onresize = function(){
-  var w = window.innerWidth, h = window.innerHeight;
-  camera.aspect = w / h;
-  camera.updateProjectionMatrix();
-  renderer.setSize( w, h );
-}
-
-cameraSpeed = .0003;
-lightSpeed = .001;
-tubularSegments = 1000;
-radialSegments = 3;
-tubeRadius = 2;
-ambientLight = 0x222222;
-lightColor = 0xffffff;
-lightIntensity = 1;
-lightDistance = 20;
-hs = 0; // Hue-Start
-he = 360; // Hue-End
-
-var renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("canvas"),
-  antialias: true,
-});
-renderer.setSize(w, h);
-
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(60, w / h, .001, 1000);
-
-var starsGeometry = new THREE.Geometry();
-for(i=0; i<3000; i++){
-	var star = new THREE.Vector3();
-	star.x = THREE.Math.randFloatSpread(1500);
-	star.y = THREE.Math.randFloatSpread(1500);
-	star.z = THREE.Math.randFloatSpread(1500);
-	starsGeometry.vertices.push(star);
-}
-var starsMaterial = new THREE.PointsMaterial({color: 0xffffff});
-var starField = new THREE.Points(starsGeometry,starsMaterial);
-scene.add(starField);
-
-for (i=0; i<p.length; i++) {
-  var x = p[i][0];
-  var y = p[i][2];
-  var z = p[i][1];
-  p[i] = new THREE.Vector3(x,y,z);
-}
-var path = new THREE.CatmullRomCurve3(p);
-var geometry = new THREE.TubeGeometry(path,tubularSegments,tubeRadius,radialSegments,true);
-
-hue = hs;
-hup = true;
-for(i=0; i<geometry.faces.length; i++){
-  hup==1?hue++:hue--;
-  hue==he?hup=false:(hue==hs)?hup=true:0;
-  geometry.faces[i].color = new THREE.Color("hsl("+hue+",100%,50%)");
-}
-
-var material = new THREE.MeshLambertMaterial({
-  side: THREE.BackSide,
-  vertexColors: THREE.FaceColors,
-  wireframe: true
+/* JavaScript Night Mode */ 
+$( ".egg-input" ).click(function() {
+  $( ".egg-label" ).toggleClass('white'),
+  $( "body" ).toggleClass('body-dark'),
+  $( ".Night-black" ).toggleClass('Night-white'),
+  $( "p" ).toggleClass('white'),
+  $( "h1" ).toggleClass('white');
+  // $("#switch").text('Switch off');
+  
 });
 
-var tube = new THREE.Mesh(geometry, material);
-scene.add(tube);
+$( ".ham-input" ).click(function() {
+  $( ".pre-header-div" ).toggleClass('pre-header-div-on'),
+  $( ".side-menu" ).toggleClass('hamburger-on');
+});
 
-var light = new THREE.PointLight(0xffffff, 1, 50);
-scene.add(light);
-var light2 = new THREE.AmbientLight(ambientLight);
-scene.add(light2);
-
-var l1 = new THREE.PointLight(lightColor, lightIntensity, lightDistance);
-scene.add(l1);
-var l2 = new THREE.PointLight(lightColor, lightIntensity, lightDistance);
-scene.add(l2);
-var l3 = new THREE.PointLight(lightColor, lightIntensity, lightDistance);
-scene.add(l3);
-var l4 = new THREE.PointLight(lightColor, lightIntensity, lightDistance);
-scene.add(l4);
-var l5 = new THREE.PointLight(lightColor, lightIntensity, lightDistance);
-scene.add(l5);
-
-var pct = 0;
-var pct2 = 0;
-function render(){
-  pct += cameraSpeed
-  pct2 += lightSpeed;
-  var pt1 = path.getPointAt(pct%1);
-  var pt2 = path.getPointAt((pct + .01)%1);
-  camera.position.set(pt1.x,pt1.y,pt1.z);
-  camera.lookAt(pt2);
-  light.position.set(pt2.x, pt2.y, pt2.z);
-  
-  l1.position.set(path.getPointAt((pct2+.0)%1).x, path.getPointAt((pct2+.0)%1).y, path.getPointAt((pct2+.0)%1).z);
-  l2.position.set(path.getPointAt((pct2+.2)%1).x, path.getPointAt((pct2+.2)%1).y, path.getPointAt((pct2+.2)%1).z);
-  l3.position.set(path.getPointAt((pct2+.4)%1).x, path.getPointAt((pct2+.4)%1).y, path.getPointAt((pct2+.4)%1).z);
-  l4.position.set(path.getPointAt((pct2+.6)%1).x, path.getPointAt((pct2+.6)%1).y, path.getPointAt((pct2+.6)%1).z);
-  l5.position.set(path.getPointAt((pct2+.8)%1).x, path.getPointAt((pct2+.8)%1).y, path.getPointAt((pct2+.8)%1).z);
-  
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
+function myFunction(x) {
+  x.classList.toggle("change");
 }
-requestAnimationFrame(render);
+
+
+
+// new 
+
+//Play with these numbers
+var radius = 30;          //Circle radius
+var weight = 3;           //Line thickness
+var force = 4000;         //Displacement sphere radius
+var maxDisplacement = 75; //Max displacement, Larger numbers mean shorter teather length
+var circleDensity = 30;   //How many circles per row
+
+var backgroundColor = 'rgb(107, 107, 107)';
+var largeCircleColor = '#222222';
+var mediumCircleColor = 'Blue';
+var smallCircleColor = 'Orange';
+
+var canvas = document.querySelector('#myCanvas');
+var ctx = canvas.getContext('2d');
+
+var height = document.getElementById("myCanvas").style.height;
+var points = [];
+
+var mouse = {
+  x: undefined,
+  y: undefined
+};
+
+canvas.margin = '0px';
+canvas.style.backgroundColor = backgroundColor;
+
+
+window.addEventListener("resize", init);
+window.addEventListener("mousemove", function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+window.addEventListener("touchmove", function(event){
+  mouse.x = event.touches[0].clientX;
+  mouse.y = event.touches[0].clientY;
+});
+window.addEventListener("mousedown", function(){ force = -force; });
+window.addEventListener("mouseup", function(){ force = -force; });
+
+
+init();
+animate();
+
+
+function animate(){
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  
+  //Loop each one separately to layer the circles correctly
+  for(var i=0; i<305; i++){
+    points[i].update();
+  }
+  
+  for(var i=0; i<305; i++){
+    points[i].drawLarge();    //Grey
+  }
+  
+  for(var i=0; i<305; i++){
+    points[i].drawMedium();   //Blue
+  }
+  
+  for(var i=0; i<305; i++){
+    points[i].drawSmall();    //Red
+  }
+}
+
+
+function Point(x, y, r){
+  this.x = x;
+  this.y = y;
+  this.radius = 30;
+  
+  this.medium = {
+    x: x,
+    y: y,
+    radius: 20
+  };
+  
+  this.small = {
+    x: x,
+    y: y,
+    radius: 10
+  };
+  
+  
+  this.drawLarge = function(){
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    ctx.strokeStyle = largeCircleColor;
+    ctx.lineWidth = weight;
+    ctx.stroke();
+  };
+  
+  this.drawMedium = function(){
+    ctx.beginPath();
+    ctx.arc(this.medium.x, this.medium.y, this.medium.radius, 0, 2 * Math.PI, false);
+    ctx.strokeStyle = mediumCircleColor;
+    ctx.lineWidth = weight * (2/3);
+    ctx.stroke();
+  }
+  
+  this.drawSmall = function(){
+    ctx.beginPath();
+    ctx.arc(this.small.x, this.small.y, this.small.radius, 0, 2 * Math.PI, false);
+    ctx.strokeStyle = smallCircleColor;
+    ctx.lineWidth = weight * (1/3);
+    ctx.stroke();
+  }
+  
+  this.update = function(){
+    var mx = mouse.x === undefined ? 10000 : mouse.x;
+    var my = mouse.y === undefined ? 10000 : mouse.y;
+    
+    //Trig
+    var mouseDist = Math.sqrt(Math.pow((mx - this.x), 2) + Math.pow((my - this.y), 2));
+    mouseDist = mouseDist < maxDisplacement ? maxDisplacement : mouseDist;
+    var angle = Math.atan( Math.abs(my - this.y) / Math.abs(mx - this.x) );
+    var a = (force / mouseDist) * Math.sin(angle);
+    var b = (force / mouseDist) * Math.cos(angle);
+    
+    if(mx > this.x){b = -b;}
+    if(my > this.y){a = -a;}
+    this.medium.x = this.x + b;
+    this.medium.y = this.y + a;
+    this.small.x = this.x + (b * 1.5);
+    this.small.y = this.y + (a * 1.5);
+  }
+  
+} //End Point
+
+
+function init(){
+  
+  points = [];
+  
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  
+  var morePoints = true;
+  var x = 0;
+  var y = 0;
+  var offset = false;
+  var dx = window.innerWidth / circleDensity;
+  dx = dx < radius ? radius : dx;
+  var dy = 2 * radius;
+    
+  
+  while(morePoints){    
+    points.push(new Point(x, y, radius));
+    x += dx;
+    if(x >= window.innerWidth + 5){
+      if(offset){
+        x = 0;
+      } else {
+        x = .5 * dx;
+      }
+      
+      offset = !offset;
+      y += dy;
+    }
+    
+    if(y > window.innerHeight){
+      morePoints = false;
+    }
+    
+  }
+  
+}
+
+
